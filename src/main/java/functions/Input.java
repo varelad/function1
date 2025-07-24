f3cb788ee5e524aee0d8f635ec4e61512c2220d3
Daniel Varela <dgvarelamartinez@gmail.com>
2025-07-24 12:47:22 -0600

    initial commit

---
+++ src/main/java/functions/Input.java
@@ -0,0 +1,20 @@
+package functions;
+
+public class Input {
+    private String message;
+
+    public Input() {}
+
+    public Input(String message) {
+        this.message = message;
+    }
+
+    public String getMessage() {
+        //test
+        return message;
+    }
+
+    public void setMessage(String message) {
+        this.message = message;
+    }
+}
